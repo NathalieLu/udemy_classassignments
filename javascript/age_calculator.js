73b98482f1728945dd8a1598e0fd067db21ab581
nathalielu <lyfd10835@gmail.com>
2020-03-12 04:45:20 +0800

    Unit 112 to 123 (Section 10.) Javascript Built-in Methods Exercises

---
+++ javascript/age_calculator.js
@@ -0,0 +1,3 @@
+var yourAge = prompt("How old are you?");
+var days = yourAge * 365.25;
+alert("You have lived for " + yourAge + " years, which is roughly " + days + " days." );
---
+++ javascript/stalker.js
@@ -0,0 +1,11 @@
+var userFirstName = prompt("What is your first name?")
+var userLastName = prompt("What is your last name?")
+var age = prompt("How old are you?")
+
+//to make it better
+var fullName = userFirstName + " " + userLastName
+
+alert("Your full name is " + fullName + ", and you are " + age + " years old.")
+
+console.log("Your full name is " + fullName + ".")
+console.log("You are " + age + " years old.")
